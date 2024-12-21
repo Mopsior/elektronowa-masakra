d2867b056a006fa6a2390acdbdce2a3f0e2b6fb7
Mopsior <bartek.szg@gmail.com>
2024-12-21 13:54:36 +0100

    Add footer padding

--- features/main/footer.tsx
+++ features/main/footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 
 export const Footer = () => {
     return (
-        <footer className="w-full bg-background text-primary py-4 text-center dark">
+        <footer className="w-full bg-background text-primary py-4 text-center dark px-2">
             <h3>Copyright {(new Date()).getFullYear()} &copy; elektronowa.xyz</h3>
             <h3 className="mb-4 flex text-center justify-center items-center">Strona stworzona przez&nbsp;<Link href={'https://github.com/mopsior'}><span className="underline underline-offset-2 deocration-1">Mopsior</span></Link><span className="text-muted ml-1 "><ExternalLink size={16} /></span></h3>
             <LegalParagraph>Serwer elektronowa.xyz nie jest w żaden sposób powiązany z firmą Microsoft oraz Mojang</LegalParagraph>
