4ed9b8cc37d94325f6bb9bfd874a1babc58e4831
Mopsior <bartek.szg@gmail.com>
2024-12-22 13:29:27 +0100

    Add static year to footer

--- features/main/footer.tsx
+++ features/main/footer.tsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 export const Footer = () => {
     return (
         <footer className="w-full bg-background text-primary py-4 text-center dark px-2">
-            <h3>Copyright {(new Date()).getFullYear()} &copy; elektronowa.xyz</h3>
+            <h3>Copyright {(new Date()).getFullYear() || '2024'} &copy; elektronowa.xyz</h3>
             <h3 className="mb-4 flex text-center justify-center items-center">Strona stworzona przez&nbsp;<Link href={'https://github.com/mopsior'}><span className="underline-animation">Mopsior</span></Link><span className="text-muted ml-1 "><ExternalLink size={16} /></span></h3>
             <LegalParagraph>Serwer elektronowa.xyz nie jest w żaden sposób powiązany z firmą Microsoft oraz Mojang</LegalParagraph>
             <LegalParagraph>Server elektronowa.xyz is not associated with Mojang or Microsoft</LegalParagraph>
