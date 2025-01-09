c7352f09f1233247086949cc4225e8c9603f8577
Mopsior <bartek.szg@gmail.com>
2025-01-09 12:16:47 +0100

    Add Hero outline (only for xl)

--- features/main/hero.tsx
+++ features/main/hero.tsx
@@ -6,7 +6,7 @@ import { OnlineMark } from "./online-mark"
 export const Hero = () => {
     return (
         <main className="px-3 text-center mt-20 md:mt-28 relative w-fit mx-auto">
-            <h1 className="uppercase text-4xl md:text-8xl font-bold tracking-wide before:content-[''] before:bg-[url('/crown.png')] before:absolute before:bg-cover before:w-16 md:before:w-24 before:h-16 md:before:h-24 before:ml-[14.5rem] md:before:ml-[23.5rem] before:top-[-45px] md:before:top-[-65px] before:z-[-1] before:rotate-6">Elektronowa<br />Masakra</h1>
+            <h1 className="uppercase text-4xl md:text-8xl font-bold tracking-wide before:content-[''] before:bg-[url('/crown.png')] before:absolute before:bg-cover before:w-16 md:before:w-24 before:h-16 md:before:h-24 before:ml-[14.5rem] md:before:ml-[23.5rem] before:top-[-45px] md:before:top-[-65px] before:z-[-1] before:rotate-6 pb-20"><span className="after:content-['Elektronowa'] after:uppercase after:absolute after:font-outline after:text-transparent after:text-[140px] after:left-[-150px] after:-top-14 after:-z-10 after:outline-to-bottom after:opacity-70">Elektronowa</span><br /><span className="after:content-['Masakra'] after:uppercase after:absolute after:font-outline after:text-transparent after:text-[140px] after:left-8 after:top-28 after:-z-10 after:outline-to-top after:opacity-70">Masakra</span></h1>
             <OnlineMark />
             <div className="mt-10 gap-x-2 md:gap-x-4 flex justify-center">
                 <CopyAddress>
